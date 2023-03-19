dc42853b4cdc888c0c73582caaa1751147d5564b
IsThatToasted <ra1nonit1@gmail.com>
2023-03-19 14:57:17 -0400

    Back to fresh
    Rollback

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import {UserContextProvider} from "./UserContext";
 import Routes from "./Routes";
 
 function App() {
-  axios.defaults.baseURL = 'https://paintersnetwork.vercel.app/api';
+  axios.defaults.baseURL = 'http://localhost:4040';
   axios.defaults.withCredentials = true;
   return (
     <UserContextProvider>
