2dc5b6599ed9a3fdac8df36a93e1368d4892a8c7
IsThatToasted <ra1nonit1@gmail.com>
2023-03-19 14:23:54 -0400

    rb
    rb

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -19,7 +19,7 @@ export default function Chat() {
     connectToWs();
   }, [selectedUserId]);
   function connectToWs() {
-    const ws = new WebSocket('ws://paintersnetwork.vercel.app/api');
+    const ws = new WebSocket('ws://localhost:4040');
     setWs(ws);
     ws.addEventListener('message', handleMessage);
     ws.addEventListener('close', () => {
